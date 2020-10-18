749843e0b5acb1c6a33a7d908450a16e5b1602ce
James Farrell <D00206780@student.dkit.ie>
2020-10-18 22:12:52 +0100

    feat (water): add simple boat controller
    boat controller script allows boats to move across a plane of water

---
+++ Assets/Scripts/BoatController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatController : MonoBehaviour
+{
+    public GameObject midLeft;
+    public GameObject midRight;
+    public float boatSpeed = 15f;
+    public float boatTurningSpeed = 5f;
+
+    private Rigidbody rigidBody;
+    private Vector3 movement;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidBody.AddForce(rigidBody.transform.forward * boatSpeed);
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rigidBody.AddForce(-rigidBody.transform.forward * boatSpeed);
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rigidBody.AddTorque(rigidBody.transform.up * boatTurningSpeed);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rigidBody.AddTorque(-rigidBody.transform.up * boatTurningSpeed);
+        }
+    }
+}

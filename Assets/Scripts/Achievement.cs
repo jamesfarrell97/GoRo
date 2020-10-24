26e38361a28f59e3086b6ae76175154327af3c91
James Farrell <D00206780@student.dkit.ie>
2020-10-24 12:53:50 +0100

    feat (achievement): add simple achievement framework
    create achievement component to manage in-game achievements
    add achievement display script which stores the location of
    available achievement slots (where achievements can be displayed)
    
    explore feasibility of achievements in progress

---
+++ Assets/Scripts/Achievement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Achievement : MonoBehaviour
+{
+    public string title;
+
+    private MeshRenderer meshRenderer;
+    
+    void Awake()
+    {
+        // Disable mesh rendered (hide model)
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.enabled = false;
+
+        // Automatically set title 
+        title = (title.Length <= 0) ? gameObject.name : title;
+    }
+
+    public void Activate(AchievementDisplay achievementDisplay)
+    {
+        // Store achievement slot
+        Transform achievementSlot = achievementDisplay.GetAchievementSlot();
+
+        // Instantiate class
+        Achievement achievement = Instantiate(this);
+
+        // Child instance to achievement slot and display mesh
+        achievement.transform.SetPositionAndRotation(achievementSlot.position, achievementSlot.rotation);
+        achievement.transform.SetParent(achievementSlot);
+        achievement.meshRenderer.enabled = true;
+    }
+}
---
+++ Assets/Scripts/AchievementDisplay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchievementDisplay : MonoBehaviour
+{
+    public Transform[] achievementSlots;
+
+    private int slotCount;
+    private int slotIndex;
+
+    void Start()
+    {
+        slotCount = achievementSlots.Length;
+        slotIndex = 0;
+    }
+
+    public Transform GetAchievementSlot()
+    {
+        if (slotIndex == slotCount)
+        {
+            slotIndex = 0;
+        }
+
+        // Index updates after variable (slotIndex) is returned
+        return achievementSlots[slotIndex++];
+    }
+}

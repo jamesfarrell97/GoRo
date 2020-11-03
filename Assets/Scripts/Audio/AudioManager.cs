dff6856509163414b0f2ed45e74ec8f4cf6c4121
James Farrell <D00206780@student.dkit.ie>
2020-11-03 12:23:59 +0000

    feat (audio): add audio manager for handling sound

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+using UnityEngine.Audio;
+using UnityEngine;
+
+// Code referenced: https://www.youtube.com/watch?v=6OT43pvUyfY
+//
+//
+//
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start()
+    {
+        Play("Theme");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+        {
+            Debug.Log("Sound: " + name + " not found!");
+            return;
+        }
+
+        s.source.Play();
+    }
+}
---
+++ Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Code referenced: https://www.youtube.com/watch?v=6OT43pvUyfY
+//
+//
+//
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    [Range(0.1f, 3f)]
+    public float pitch;
+
+    [HideInInspector]
+    public AudioSource source;
+
+    public bool loop;
+}

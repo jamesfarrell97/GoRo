ad86a7b1b07f559fe7c84c134c3617d2258510f8
James Farrell <D00206780@student.dkit.ie>
2020-12-08 12:33:02 +0000

    feat (bluetooth): create BLE connection interface
    allows for connection with BLE devices through a simple UI interface
    
    integrate with concept2 to test

--- Assets/Scripts/IO Bus/Blue.cs
+++ Assets/Scripts/IO Bus/Blue.cs
@@ -1,118 +1,195 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-using InTheHand.Net;
-using InTheHand.Net.Bluetooth;
-using InTheHand.Net.Sockets;
-using InTheHand.Bluetooth;
+using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
 
 public class Blue : MonoBehaviour
 {
-    void Awake()
+    [SerializeField] Transform deviceListContent;
+    [SerializeField] GameObject deviceListItemPrefab;
+    public TMP_Text log;
+
+    public List<Text> Buttons;
+    public List<string> Services;
+    public List<string> Characteristics;
+
+    public void OnSearchClick()
     {
-        Debug.Log("1");
+        log.text = "Log: ";
+
+        ResetList();
+
+        BluetoothLEHardwareInterface.Initialize(true, false, () => {
+            
+            FoundDeviceListScript.DeviceAddressList = new List<DeviceObject>();
+            
+            BluetoothLEHardwareInterface.ScanForPeripheralsWithServices(null, (address, name) => {
+
+                DeviceObject dObj = new DeviceObject(address, name);
+
+                FoundDeviceListScript.DeviceAddressList.Add(dObj);
+                UpdateDeviceList(dObj);
+
+            }, null);
+
+        }, (error) => {
+            
+            BluetoothLEHardwareInterface.Log("BLE Error: " + error);
+
+        });
     }
 
-    void Start()
+    private void ResetList()
     {
-        Debug.Log("A");
-        StartCoroutine(Setup());
-        StartCoroutine(BLEScan());
-    }
-
-    IEnumerator Setup()
-    {
-        var requestDeviceOptions = new RequestDeviceOptions
+        DeviceListItem.DeviceListItemID = 0;
+        foreach (Transform transform in deviceListContent)
         {
-            AcceptAllDevices = true
-        };
-        var devices = Bluetooth.ScanForDevicesAsync(requestDeviceOptions);
-
-        Debug.Log(devices.ToString());
-
-        yield return null;
-    }
-
-    IEnumerator BLEScan()
-    {
-        Debug.Log("2");
-        var client = new BluetoothClient();
-        var devices = client.DiscoverDevices();
-        var bluetoothClient = new BluetoothClient();
-
-        Debug.Log("3");
-        string authenticated;
-        string classOfDevice;
-        string connected;
-        string deviceAddress;
-        string deviceName;
-        string installedServices;
-
-        Debug.Log("PD: " + client.PairedDevices.First().DeviceName);
-        Debug.Log("D: " + client.DiscoverDevices());
-        Debug.Log("BD: " + bluetoothClient.DiscoverDevices());
-
-        Debug.Log("4");
-        foreach (BluetoothDeviceInfo device in devices)
-        {
-            authenticated = device.Authenticated.ToString();
-            classOfDevice = device.ClassOfDevice.ToString();
-            connected = device.Connected.ToString();
-            deviceAddress = device.DeviceAddress.ToString();
-            deviceName = device.DeviceName.ToString();
-            installedServices = device.InstalledServices.ToString();
-
-            string[] row = new string[] { authenticated, classOfDevice, connected, deviceAddress, deviceName, installedServices/*, lastSeen, lastUsed, remembered, rssi */ };
-
-            Debug.Log("Device Name:" + row[4] + " || MAC Address:" + row[3]);
+            Destroy(transform.gameObject);
         }
-
-        Debug.Log("5");
-
-        Console.WriteLine("Scan Complete");
-
-        yield return null;
     }
 
-}
-
-/*
-    lsb_device.Items.Clear();
-    BluetoothRadio.PrimaryRadio.Mode = RadioMode.Connectable;
-    BluetoothClient client = new BluetoothClient();
-    BluetoothDeviceInfo[] devices = client.DiscoverDevices();
-    BluetoothClient bluetoothClient = new BluetoothClient();
-    String authenticated;
-    String classOfDevice;
-    String connected;
-    String deviceAddress;
-    String deviceName;
-    String installedServices;
-    String lastSeen;
-    String lastUsed;
-    String remembered;
-    String rssi;
-    foreach (BluetoothDeviceInfo device in devices)
+    public void OnSubscribeClick(DeviceListItem listItem)
     {
-        lbl_status.Visible = true;
-        authenticated = device.Authenticated.ToString();
-        classOfDevice = device.ClassOfDevice.ToString();
-        connected = device.Connected.ToString();
-        deviceAddress = device.DeviceAddress.ToString();
-        deviceName = device.DeviceName.ToString();
-        installedServices = device.InstalledServices.ToString();
-        lastSeen = device.LastSeen.ToString();
-        lastUsed = device.LastUsed.ToString();
-        remembered = device.Remembered.ToString();
-        rssi = device.Rssi.ToString();
-        string[] row = new string[] { authenticated, classOfDevice, connected, deviceAddress, deviceName, installedServices, lastSeen, lastUsed, remembered, rssi };
+        log.text += "S";
+        Debug.Log("S");
 
-        lsb_device.Items.Add("Device Name:" + row[4] + " || MAC Address:" + row[3]);
+        DeviceObject device = FoundDeviceListScript.DeviceAddressList[listItem.DeviceID];
+
+        string subscribedService = Services[listItem.DeviceID];
+        string subscribedCharacteristic = Characteristics[listItem.DeviceID];
+
+        log.text += "O";
+        Debug.Log("O");
+
+        if (!string.IsNullOrEmpty(subscribedService) && !string.IsNullOrEmpty(subscribedCharacteristic))
+        {
+            log.text += "Y";
+            Debug.Log("Y");
+
+            BluetoothLEHardwareInterface.Log("subscribing to: " + subscribedService + ", " + subscribedCharacteristic);
+
+            log.text += "SC: " + subscribedService + ", " + subscribedCharacteristic + " ";
+            Debug.Log("SC: " + subscribedService + ", " + subscribedCharacteristic + " ");
+
+            BluetoothLEHardwareInterface.SubscribeCharacteristic(device.Address, subscribedService, subscribedCharacteristic, null, (characteristic, bytes) => {
+
+                BluetoothLEHardwareInterface.Log("received data: " + characteristic);
+
+                log.text += "RD: " + characteristic + " ";
+                Debug.Log("RD: " + characteristic + " ");
+            });
+        }
+        else
+        {
+            log.text += "!Y";
+            Debug.Log("!Y");
+        }
     }
-    lbl_status.Text = "Scan completed.";
-*/
+
+    public void OnButtonClick(DeviceListItem deviceListItem)
+    {
+        log.text += "B";
+        Debug.Log("B");
+
+        DeviceObject device = FoundDeviceListScript.DeviceAddressList[deviceListItem.DeviceID];
+
+        string subscribedService = Services[deviceListItem.DeviceID];
+        string subscribedCharacteristic = Characteristics[deviceListItem.DeviceID];
+
+        log.text += "C";
+        Debug.Log("C");
+
+        if (device != null)
+        {
+            log.text += "!N";
+            Debug.Log("!N");
+
+            if (deviceListItem.DeviceStatus.text.Equals("Connected"))
+            {
+                log.text += "C1";
+                Debug.Log("C1");
+
+                if (!string.IsNullOrEmpty(subscribedService) && !string.IsNullOrEmpty(subscribedCharacteristic))
+                {
+                    log.text += "A0";
+                    Debug.Log("A0");
+
+                    BluetoothLEHardwareInterface.UnSubscribeCharacteristic(device.Address, subscribedService, subscribedCharacteristic, (characteristic) => {
+
+                        log.text += "A1";
+                        Debug.Log("A1");
+
+                        Services[deviceListItem.DeviceID] = null;
+                        Characteristics[deviceListItem.DeviceID] = null;
+
+                        BluetoothLEHardwareInterface.DisconnectPeripheral(device.Address, (disconnectAddress) => {
+
+                            log.text += "A2";
+                            Debug.Log("A2");
+
+                            deviceListItem.DeviceStatus.text = "Disconnected";
+                        });
+                    });
+
+                    log.text += "D1";
+                    Debug.Log("D1");
+                }
+                else
+                {
+                    BluetoothLEHardwareInterface.DisconnectPeripheral(device.Address, (disconnectAddress) => {
+
+                        deviceListItem.DeviceStatus.text = "Disconnected";
+                    });
+
+                    log.text += "D2";
+                    Debug.Log("D2");
+                }
+            }
+            else
+            {
+                BluetoothLEHardwareInterface.ConnectToPeripheral(device.Address, (address) => {
+
+                }, null, (address, service, characteristic) => {
+
+                    if (string.IsNullOrEmpty(Services[deviceListItem.DeviceID]) && string.IsNullOrEmpty(Characteristics[deviceListItem.DeviceID]))
+                    {
+                        Services[deviceListItem.DeviceID] = FullUUID(service);
+                        Characteristics[deviceListItem.DeviceID] = FullUUID(characteristic);
+                        deviceListItem.DeviceStatus.text = "Connected";
+                    }
+
+                }, null);
+
+                log.text += "D0";
+                Debug.Log("D0");
+            }
+        }
+        else
+        {
+            log.text += "N";
+            Debug.Log("N");
+        }
+    }
+
+    private void UpdateDeviceList(DeviceObject dObj)
+    {
+        Instantiate(deviceListItemPrefab, deviceListContent).GetComponent<DeviceListItem>().SetUp(dObj);
+        
+        Services.Add(null);
+        Characteristics.Add(null);
+    }
+
+    private void OnCharacteristic(string characteristic, byte[] bytes)
+    {
+        BluetoothLEHardwareInterface.Log("received: " + characteristic);
+    }
+
+    private string FullUUID(string uuid)
+    {
+        if (uuid.Length == 4)
+            return "0000" + uuid + "-0000-1000-8000-00805f9b34fb";
+
+        return uuid;
+    }
+}
---
+++ Assets/Scripts/UI/DeviceListItem.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+using TMPro;
+
+public class DeviceListItem : MonoBehaviour
+{
+    public static int DeviceListItemID = 0;
+
+    public int DeviceID;
+    public TMP_Text DeviceName;
+    public TMP_Text DeviceAddress;
+    public TMP_Text DeviceStatus;
+
+    private Blue Blue;
+
+    public void SetUp(DeviceObject device)
+    {
+        Blue = FindObjectOfType<Blue>();
+
+        DeviceID = DeviceListItemID;
+
+        DeviceName.text = device.Name;
+        DeviceAddress.text = device.Address;
+
+        DeviceListItemID++; 
+    }
+
+    public void OnButtonClick(DeviceListItem deviceListItem)
+    {
+        Blue.OnButtonClick(deviceListItem);
+    }
+
+    public void OnSubscribeClick(DeviceListItem deviceListItem)
+    {
+        Blue.OnSubscribeClick(deviceListItem);
+    }
+
+}

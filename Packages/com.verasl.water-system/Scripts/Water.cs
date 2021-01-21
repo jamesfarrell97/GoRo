2b30c0c278488c37ce42bdec961e55751eea1c6f
James Farrell <D00206780@student.dkit.ie>
2021-01-21 14:19:10 +0000

    world: create game environment
    update environment done

--- Packages/com.verasl.water-system/Scripts/Water.cs
+++ Packages/com.verasl.water-system/Scripts/Water.cs
@@ -23,6 +23,7 @@ namespace WaterSystem
                 return _instance;
             }
         }
+
         // Script references
         private PlanarReflections _planarReflections;
 
@@ -344,8 +345,7 @@ namespace WaterSystem
             //Generate the camera
             if(_depthCam == null)
             {
-                var go =
-                    new GameObject("depthCamera") {hideFlags = HideFlags.HideAndDontSave}; //create the cameraObject
+                var go = new GameObject("depthCamera") {hideFlags = HideFlags.HideAndDontSave}; //create the cameraObject
                 _depthCam = go.AddComponent<Camera>();
             }
 
@@ -378,7 +378,7 @@ namespace WaterSystem
             _depthTex.name = "WaterDepthMap";
             //do depth capture
             _depthCam.targetTexture = _depthTex;
-            _depthCam.Render();
+            //_depthCam.Render();
             Shader.SetGlobalTexture(WaterDepthMap, _depthTex);
             // set depth bufferParams for depth cam(since it doesnt exist and only temporary)
             var _params = new Vector4(t.position.y, 250, 0, 0);
